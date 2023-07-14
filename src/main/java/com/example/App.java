749f98377d7961c47148e5121e9c7ba7f92a57b5
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-14 12:20:47 +0200

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        int sum = app.add(2, 3);
+        int difference = app.subtract(5, 3);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
+}
+
+
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+// import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testAdd() {
+        App app = new App();
+        int result = app.add(2, 3);
+        Assert.assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        App app = new App();
+        int result = app.subtract(5, 3);
+        Assert.assertEquals(2, result);
+    }
+}
